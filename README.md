7799d27e878bd2de342fdcf7111d5eb91b415b77
Fernando Brunelli Costa <fernando@casetecnologia.com.br>
2019-09-30 08:20:11 -0300

    Translating to english.

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 # partitiontables_zabbix
 ```
-This is a scripts for partitiontables of MySQL zabbix version is 2.2 3.0 3.2 3.4 4.0
-support Zabbix 3.0 4.0
-MySQL  version 5.6  5.7 8.0 
+This is a script for table partitioning for Zabbix database on MySQL.
+Supports Zabbix 2.2, 3.0, 3.2, 3.4 and 4.0, MySQL version 5.6, 5.7 and 8.0 
 ``` 
-本项目是《Zabbix企业级分布式监控系统》第2版中第3章节中的代码，如需使用，请注明出处即可，遵循Apache 2.0开源协议     
+This project is the code provided in the third chapter of the second edition of Zabbix Enterprise Distributed Monitoring System. If you need to use it, please indicate the source and follow the Apache 2.0 open source license.
 
-# 1. configration 
-Before run this scripts,maybe you should modify it   
-修改脚本中的账号信息，如下所示
+# 1. configuration
+Before running this script, you should adjust the database settings and the data retention policy
 ```
 # MySQL connect information
 ZABBIX_USER="zabbix"
@@ -18,14 +16,14 @@ ZABBIX_PORT="3306"
 ZABBIX_HOST="127.0.0.1"
 MYSQL_BIN="mysql"
 
-# How days you will keep history days,default is 30  历史数据存储保留时间
+# For how many days you will keep history data (30 by default)
 HISTORY_DAYS=30
 
-# How months you will keep trend days,default is 12  趋势数据存储保留时间
+# For how many months you will keep trend data (12 by default)
 TREND_MONTHS=12
 ```
 # 2. run it
-运行脚本     
+Running the script  
 ```
 shell# bash partitiontables_zabbix.sh 
 table history      create partitions p20180716
@@ -45,7 +43,7 @@ table trends       create partitions p201808
 table trends_uint  create partitions p201808
 ```
 # 3. check partitions
-验证脚本是否执行成功  
+Verify that the script executed successfully
 ```
 shell# mysql -uzabbix -pzabbix zabbix
 MariaDB [zabbix]> show create table history\G;
@@ -68,34 +66,16 @@ PARTITION p20180721 VALUES LESS THAN (1532188799) ENGINE = InnoDB,
 PARTITION p20180722 VALUES LESS THAN (1532275199) ENGINE = InnoDB,
 PARTITION p20180723 VALUES LESS THAN (1532361599) ENGINE = InnoDB) */
 1 row in set (0.00 sec)
-ERROR: No query specified
 MariaDB [zabbix]>
 ```
-当看到PARTITION的时候，说明分区已创建成功。  
-if you see this,everything is OK.
+If you see PARTITION, the partitions have been created successfully. 
+Everything is OK.
 
 # 4. set crontab
-the crontab remove old partitions every day    
-每天的定时任务，是由linux的crontab去实现的，需确保Linux服务器的crond启动成功。否则会引起新分区无法自动创建的问题   
+Regular daily tasks are implemented using linux crontab, make sure crond has started successfully.
+Otherwise new partitions will not be created automatically.
 ```
 shell# crontab -e
 1 0 * * * /usr/sbin/partitiontables_zabbix.sh
 Shell# chmod 700 /usr/sbin/partitiontables_zabbix.sh
-```
-# truncate table(only with delete data)
-if you database is so big,you should clean data first,then run this scripts   
-
-当运行此脚本的时候，Zabbix库有存量数据，此时，建议清空想表的数据，然后再执行此脚本   
-```
-mysql> use zabbix; 
-mysql> truncate table history; 
-mysql> optimize table history; 
-mysql> truncate table history_str; 
-mysql> optimize table history_str; 
-mysql> truncate table history_uint; 
-mysql> optimize table history_uint; 
-mysql> truncate table trends; 
-mysql> optimize table trends; 
-mysql> truncate table trends_uint; 
-mysql> optimize table trends_uint; 
 ```
--- partitiontables_zabbix.sh
+++ partitiontables_zabbix.sh
@@ -6,7 +6,7 @@
 # funtion: create parition for zabbix MySQL 
 # repo: https://github.com/zabbix-book/partitiontables_zabbix
 
-#配置环境变量
+# Configuring environment variables
 ZABBIX_USER="zabbix"
 ZABBIX_PWD="zabbix"
 ZABBIX_DB="zabbix"
@@ -14,20 +14,20 @@ ZABBIX_PORT="3306"
 ZABBIX_HOST="127.0.0.1"
 MYSQL_BIN="mysql"
 
-#历史数据保留时间，单位天
+# Historical data retention time in days
 HISTORY_DAYS=30
 
-#趋势数据保留时间，单位月
+# Trend data retention time in months
 TREND_MONTHS=12
 
 HISTORY_TABLE="history history_log history_str history_text history_uint"
 TREND_TABLE="trends trends_uint"
 
-#MYSQL连接命令
+# MYSQL connection command
 MYSQL_CMD=$(echo ${MYSQL_BIN} -u${ZABBIX_USER} -p${ZABBIX_PWD} -P${ZABBIX_PORT} -h${ZABBIX_HOST} ${ZABBIX_DB})
 
 function create_partitions_history() {
-    #给历史表创建分区
+    # Create a partition for the history table
     for PARTITIONS_CREATE_EVERY_DAY in $(date +"%Y%m%d") $(date +"%Y%m%d" --date='1 days') $(date +"%Y%m%d" --date='2 days') $(date +"%Y%m%d" --date='3 days')  $(date +"%Y%m%d" --date='4 days') $(date +"%Y%m%d" --date='5 days') $(date +"%Y%m%d" --date='6 days') $(date +"%Y%m%d" --date='7 days')
     do
         TIME_PARTITIONS=$(date -d "$(echo ${PARTITIONS_CREATE_EVERY_DAY} 23:59:59)" +%s)
@@ -35,7 +35,7 @@ function create_partitions_history() {
         do
             SQL1=$(echo "show create table ${TABLE_NAME};")
             RET1=$(${MYSQL_CMD} -e "${SQL1}"|grep "PARTITION BY RANGE"|wc -l)
-            #表结构中的表分区不存在，则创建
+            # There is no table partition in the table structure, create
             if [ "${RET1}" == "0" ];then
                 SQL2=$(echo "ALTER TABLE $TABLE_NAME PARTITION BY RANGE( clock ) (PARTITION p${PARTITIONS_CREATE_EVERY_DAY}  VALUES LESS THAN (${TIME_PARTITIONS}));")
                 RET2=$(${MYSQL_CMD} -e "${SQL2}")
@@ -47,7 +47,7 @@ function create_partitions_history() {
                 fi
                 continue
             fi
-            #表结构中的表分区已经存在，则创建分区
+            # The table partition in the table structure already exists, create a partition
             if [ "${RET1}" != "0" ];then
                 SQL3=$(echo "show create table ${TABLE_NAME};")
                 RET3=$(${MYSQL_CMD} -e "${SQL3}"|grep "p${PARTITIONS_CREATE_EVERY_DAY}"|wc -l)
@@ -68,7 +68,7 @@ function create_partitions_history() {
 }
 
 function drop_partitions_history() {
-    #删除历史表分区
+    # Delete history table partition
     for PARTITIONS_DELETE_DAYS_AGO in $(date +"%Y%m%d" --date="${HISTORY_DAYS} days ago")
     do
         for TABLE_NAME in ${HISTORY_TABLE}
@@ -90,7 +90,7 @@ function drop_partitions_history() {
 }
 
 function create_partitions_trend() {
-    #创建趋势表分区
+    # Create trend table partition
     for PARTITIONS_CREATE_EVERY_MONTHS in $(date +"%Y%m") $(date +"%Y%m" --date='1 months') $(date +"%Y%m" --date='2 months') $(date +"%Y%m" --date='3 months') $(date +"%Y%m" --date='4 months') $(date +"%Y%m" --date='5 months')
     do
         TIME_PARTITIONS=$(date -d "$(echo ${PARTITIONS_CREATE_EVERY_MONTHS}01 00:00:00)" +%s)
@@ -98,7 +98,7 @@ function create_partitions_trend() {
         do
             SQL1=$(echo "show create table ${TABLE_NAME};")
             RET1=$(${MYSQL_CMD} -e "${SQL1}"|grep "PARTITION BY RANGE"|wc -l)
-            #表结构中的表分区不存在，则创建
+            # There is no table partition in the table structure, create
             if [ "${RET1}" == "0" ];then
                 SQL2=$(echo "ALTER TABLE $TABLE_NAME PARTITION BY RANGE( clock ) (PARTITION p${PARTITIONS_CREATE_EVERY_MONTHS}  VALUES LESS THAN (${TIME_PARTITIONS}));")
                 RET2=$(${MYSQL_CMD} -e "${SQL2}")
@@ -110,7 +110,7 @@ function create_partitions_trend() {
                 fi
                 continue
             fi
-            #表结构中的表分区已经存在，则创建分区
+            # The table partition in the table structure already exists, create a partition
             if [ "${RET1}" != "0" ];then
                 SQL3=$(echo "show create table ${TABLE_NAME};")
                 RET3=$(${MYSQL_CMD} -e "${SQL3}"|grep "p${PARTITIONS_CREATE_EVERY_MONTHS}"|wc -l)
@@ -131,7 +131,7 @@ function create_partitions_trend() {
 }
 
 function drop_partitions_trend() {
-    #删除趋势表分区
+    # Delete trend table partition
     for PARTITIONS_DELETE_MONTHS_AGO in $(date +"%Y%m" --date="${TREND_MONTHS} months ago")
     do
         for TABLE_NAME in ${TREND_TABLE}
